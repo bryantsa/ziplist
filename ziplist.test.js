8d634590bc597b40cd0257fbb5feadb7719778d9
bryantsa <31455655+bryantsa@users.noreply.github.com>
2017-11-19 22:03:23 -1000

    Update ziplist.test.js

--- ziplist.test.js
+++ ziplist.test.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha, chai */
+/* global zipList,zipListTheSimpleWay chai */
 
 describe('ZipList', function () {
   const l = ['a', 'b', 'c'];
@@ -6,5 +8,10 @@ describe('ZipList', function () {
     it('should return an array containing both variables for zipList', function () {
       chai.expect(zipList(l,o)).to.equal(["a", 1, "b", 2, "c", 3]);
       chai.expect(zipList(l,o)).to.be.an('array');
+    });
+    describe('zipListTheSimpleWay()', function () {
+    it('should return an array containing both variables for zipList', function () {
+      chai.expect(zipListTheSimpleWay(l,o)).to.equal(["a", 1, "b", 2, "c", 3]);
+      chai.expect(zipListTheSimpleWay(l,o)).to.be.an('array');
     });
   });
