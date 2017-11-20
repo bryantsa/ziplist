3bf46c87a53b14a6b5a63d8e2d9b5a0fd781e9e2
bryantsa <31455655+bryantsa@users.noreply.github.com>
2017-11-19 21:57:59 -1000

    Update ziplist.test.js

--- ziplist.test.js
+++ ziplist.test.js
@@ -1,9 +1,9 @@
 
 describe('ZipList', function () {
-  const l = ['a','b','c'];
+  const l = ['a', 'b', 'c'];
   const o = [1, 2, 3];
   describe('zipList()', function () {
-    it('should return 10 for testnums', function () {
-      chai.expect(sumFor(testNums)).to.equal(10);
+    it('should return an array containing both variables for zipList', function () {
+      chai.expect(zipList(l,o)).to.equal(["a", 1, "b", 2, "c", 3]);
     });
   });
